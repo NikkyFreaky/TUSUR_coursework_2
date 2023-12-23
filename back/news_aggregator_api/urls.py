8c9f443035d9513b952111270f36558bff72a004
Having-no-guise <eskelanir@mail.ru>
2023-12-23 20:11:02 +0700

    Сделано: парсер работает по странам

--- back/news_aggregator_api/urls.py
+++ back/news_aggregator_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('scrape/', views.scrape, name='scrape'),
     path('', views.default),
+    path('get_news_by_country/', views.get_news_by_country, name='get_news_by_country'),  # эндпоинт для вывода новостей
 ]
--- back/news_aggregator_api/views.py
+++ back/news_aggregator_api/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render, redirect
 from .components.parser import parse_news
-
+from .models import News, Country
+from django.http import JsonResponse
 # Create your views here.
 
+
+def get_news_by_country(request):  # сам эндпоинт
+
+    if request.method == 'GET':
+        country_name = request.GET.get('country_name')
+        try:
+            parse_news(country_name)
+            country = Country.objects.get(country_name=country_name)
+            news_by_country = News.objects.filter(countries=country)
+            news_data = [
+                {
+                    'title': news.title,
+                    'description': news.description,
+                    'event_date': news.event_date,
+                    'publication_date': news.publication_date,
+                    # добавить ругие поля которые надо выдать на фронт
+                }
+                for news in news_by_country
+            ]
+            return JsonResponse({'news': news_data})
+        except Country.DoesNotExist:
+            return JsonResponse({'error': 'Страна не найдена'}, status=404)
+    return redirect("../")
+
+
 def scrape(request):
-    parse_news()
+    if request.method == 'GET':
+        country_name = request.GET.get('country_name')
+        parse_news(country_name)
     return redirect("../")
 
 
